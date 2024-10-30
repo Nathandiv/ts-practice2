d15a0551ea694340916ac1b36edf1afb7cf05fac
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-30 15:44:45 +0200

    did mini Atm

--- index.ts
+++ index.ts
@@ -1,22 +1,60 @@
-class User{
-    name:string;
-    age:number;
+// class User{
+//     name:string;
+//     age:number;
 
-    constructor( name:string, age:number){
-        this.name = name;
-        this.age = age;
+//     constructor( name:string, age:number){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     isAdult():boolean{
+//         if (this.age >=18 ) {
+//             return true;
+            
+//         }else{
+//             return false;
+//         }
+//     }
+
+// }
+
+// const user1=new User('Nathan',30)
+// console.log(user1.isAdult());
+class BankAcoount{
+    accountNumber:number;
+    balance:number;
+
+    constructor( account:number, initialBlalnce:number){
+        this.accountNumber = account;
+        this.balance = initialBlalnce;
     }
 
-    isAdult():boolean{
-        if (this.age >=18 ) {
-            return true;
-            
-        }else{
-            return false;
-        }
+    deposit(amount:number):void{
+        this.balance += amount;
+        console.log(`you deposit was succesfull: current balance R${this.balance}`);
+    }
+
+    withdraw(amount:number):void{
+       if ( this.balance += amount
+        ) {
+            this.balance -= amount
+            console.log(`you deposit was succesfull: current balance R${this.balance}`);
+       }else {
+        console.log(`insufficient funds`);
+       }
+
     }
 
 }
 
-const user1=new User('Nathan',30)
-console.log(user1.isAdult());
+
+const account = new BankAcoount(11123,500)
+// account.deposit(1000)
+account.withdraw(300)
+
+
+
+
+
+
+
