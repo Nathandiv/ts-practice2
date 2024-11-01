08343914de38e47be49979b2f4210d055245cca3
Nathandiv <k.n.hlungwani@gmail.com>
2024-11-01 09:36:36 +0200

    done with second task

--- index.ts
+++ index.ts
@@ -20,37 +20,71 @@
 
 // const user1=new User('Nathan',30)
 // console.log(user1.isAdult());
-class BankAcoount{
-    accountNumber:number;
-    balance:number;
+// class BankAcoount{
+//     accountNumber:number;
+//     balance:number;
 
-    constructor( account:number, initialBlalnce:number){
-        this.accountNumber = account;
-        this.balance = initialBlalnce;
-    }
+//     constructor( account:number, initialBlalnce:number){
+//         this.accountNumber = account;
+//         this.balance = initialBlalnce;
+//     }
 
-    deposit(amount:number):void{
-        this.balance += amount;
-        console.log(`you deposit was succesfull: current balance R${this.balance}`);
-    }
+//     deposit(amount:number):void{
+//         this.balance += amount;
+//         console.log(`you deposit was succesfull: current balance R${this.balance}`);
+//     }
 
-    withdraw(amount:number):void{
-       if ( this.balance >= amount
-        ) {
-            this.balance -= amount
-            console.log(`withdrawal was succesfull: current balance R${this.balance}`);
-       }else {
-        console.log(`insufficient funds`);
-       }
+//     withdraw(amount:number):void{
+//        if ( this.balance >= amount
+//         ) {
+//             this.balance -= amount
+//             console.log(`withdrawal was succesfull: current balance R${this.balance}`);
+//        }else {
+//         console.log(`insufficient funds`);
+//        }
 
-    }
+//     }
 
+// }
+
+
+// const account = new BankAcoount(11123,500)
+// account.deposit(1000)
+// account.withdraw(300)
+
+class Products {
+   name:string;
+   price:number;
+   stock:number;
+   constructor (name:string,price:number,stock:number){
+    this.name =name;
+    this.price =price;
+    this.stock =stock;
+   }
+
+   isAvailable():boolean{
+if (this.stock >0){
+    return true;
+}else{
+    return false ;
 }
+   }
 
+   purchase(quantity:number):string{
+    if (this.stock >= quantity) {
+        this.stock -= quantity
+        return `purchase successfull ${quantity} ${this.name}(s) bought remaining stock ${this.stock}`
+        
+    }else {
+        return `Insufficient stock for the required quantity `
+    }
+   }
+}
+ const product = new Products("Car",65000,10)
+ console.log(product.isAvailable());
+ console.log(product.purchase(3));
+ console.log(product.purchase(2));
 
-const account = new BankAcoount(11123,500)
-account.deposit(1000)
-account.withdraw(300)
 
 
 
